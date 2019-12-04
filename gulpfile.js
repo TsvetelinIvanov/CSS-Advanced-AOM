e56b52901e066a905355009fb5f6bd4e955d7a9c
TsvetelinIvanov <tzendy01@abv.bg>
2019-12-04 22:49:13 +0200

    Update footer and home page, and add new images

--- gulpfile.js
+++ gulpfile.js
@@ -56,7 +56,7 @@ gulp.task("watch", function() {
 });
 
 gulp.task("deploy", gulp.series('jekyll', 'sass', function() {
-	return cp.spawn('git status && git commit -am "Update header and add quasi-logo" && git pull && git push', { stdio: "inherit", shell: true });
+	return cp.spawn('git status && git commit -am "Update footer and home page, and add new images" && git pull && git push', { stdio: "inherit", shell: true });
 }));
 
 gulp.task("default", gulp.series('jekyll-dev', 'sass', 'watch'));
